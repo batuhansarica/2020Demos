cbb812b27863ee103bf52f444049f27b3da58aab
batuhansarica <73408243+batuhansarica@users.noreply.github.com>
2021-10-20 17:46:14 +0300

    https://github.com/batuhansarica/companyintroduction.git

---
+++ vuejs/project/example/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Navbar></Navbar>
+      
+
+      
+     
+    </div>
+    
+    
+   
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+    
+  },
+}
+
+    </script>
+
+
+
+<style lang="scss">
+
+</style>
---
+++ vuejs/project/example/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+<Slide></Slide>
+  
+</template>
+
+<script>
+
+
+
+import Slide from "@/components/Slide.vue";
+export default {
+  name: "Home",
+  components: {
+    Slide,
+    
+  },
+};
+</script>
